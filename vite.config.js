c33ca7ecfeeec156cd0a8889771671ca666abca8
Tanzeel Ahmed <tanzeelahmed412@gmail.com>
2025-11-29 15:48:39 +0500

    docs

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    host: true, // allow access from network (mobile/other devices on LAN)
     open: true
   }
 })
